b1f59a4954425509f29453b6a42923ac5d12e9ea
tarom24 <wahyuraj3@gmail.com>
2025-09-18 20:28:23 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||You|Are|My|Love||#rectangle|");
+      S.UI.simulate("|#countdown 3||aku|mau|minta maaf|sayanggkuu||#rectangle|");
     }
 
     // Add a flag to track animation completion
